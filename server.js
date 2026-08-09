7358b65d30a44c642aabebe0a416e0dee6150e9a
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-08-09 22:09:53 +0600

    cors

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
-require('dotenv').config()
-const express = require ('express')
-const router = require ('./routes')
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
 const cookieParser = require("cookie-parser");
-const dbConfig = require('./configs/dbConfig')
-const PORT = process.env.PORT || 88800
+const dbConfig = require("./configs/dbConfig");
 
-// // -----when db congfig not working then use this code t
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4'])
+const PORT = process.env.PORT || 8000;
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(router)
-// app.use(limiter)
-dbConfig()
+// ------dns 
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+const app = express();
 
+// ---------- CORS ----------
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
+// ---------- Middleware 
+app.use(express.json());
+app.use(cookieParser());
+
+// ---------- Routes 
+app.use(router);
+
+// ---------- Database 
+dbConfig();
+
+// ---------- Server 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
